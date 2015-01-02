1ea18df3f45073fac720e49d1c125a66de49f0af
Rayees <p_rayees@hotmail.co.uk>
2015-01-02 00:58:53 +0000

    making corrections

--- app/controllers/ImageController.php
+++ app/controllers/ImageController.php
@@ -29,12 +29,12 @@
 		    //(In ideal application, you should check the filename to be unique)
 		    $fullname = Str::slug(Str::random(8).$filename).'.'.$image->getClientOriginalExtension();
 		    //We upload the image first to the upload folder, then get make a thumbnail from the uploaded image
-		    $upload = $image->move(Config::get( 'image.upload_folder'), $fullname);
+		    $upload = $image->move(Config::get( 'Image.upload_folder'), $fullname);
 		    //Our model that we've created is named Photo, this library has an alias named Image, don't mix them two!
 		    //These parameters are related to the image processing class that we've included, not really related to Laravel
-		    Image::make(Config::get( 'image.upload_folder').'/'.$fullname)
-		    	->resize(Config::get( 'image.thumb_width'), null, true)
-		        ->save(Config::get('image.thumb_folder').'/'.$fullname);
+		    Image::make(Config::get('Image.upload_folder').'/'.$fullname)
+		    	->resize(Config::get('Image.thumb_width', 'Image.thumb_height')
+		        ->save(Config::get('Image.thumb_folder').'/'.$fullname)
 		    //If the file is not uploaded, we show an error message to the user, else we add a new column to the database and show the success message
 		    if($upload) {
 		    	//image is now uploaded, we first need to add column to the database
@@ -52,5 +52,49 @@
 			    	->with('error','Sorry, the image could not be uploaded, please try again later');
 			}
 		}
+	}
+
+	public function getSnatch($id)
+	{
+		//Let's try to find the image from database first
+        $image = Photo::find($id);
+        //If found, we load the view and pass the image info as parameter, else we redirect to main page with error message
+        if($image) {
+        	return View::make('tpl.permalink')
+            	->with('image', $image);
+        } else {
+        	return Redirect::to('/')
+        		->with('error', 'Image not found');
+        }
+    }
+
+    public function getAll()
+    {
+    	//Let's first take all images with a pagination feature
+    	$all_images = DB::table('photos')->orderBy('id','desc')->paginate(6);
+     	//Then let's load the view with found data and pass the variable to the view
+    	return View::make('tpl.all_images')
+       		->with('images',$all_images);
+	}
+
+	public function getDelete($id)
+	{
+		//Let's first find the image
+        $image = Photo::find($id);
+        //First, let's delete the images from FTP
+		File::delete(Config::get('image.upload_folder').'/'.$image->image);
+		File::delete(Config::get('image.thumb_folder').'/'.$image->image);
+		//Now let's delete the value from database
+		$deleted = $image->delete();
+
+		if($deleted) {
+			//Let's return to the main page with a success message
+           	return Redirect::to('/')
+            	->with('success','Image deleted successfully');
+		} else {
+        	//Image not found, so we will redirect to the index page with an error message flash data.
+        	return Redirect::to('/')
+            	->with('error','No image with given ID found');
+        }
 	}
 }
--- app/routes.php
+++ app/routes.php
@@ -12,7 +12,16 @@
 */
 
 //This is for the get event of the index page
-Route::get('/',array('as' => 'index_page', 'uses' => 'ImageController@getIndex'));
+Route::get('/', array('as' => 'index_page', 'uses' => 'ImageController@getIndex'));
 
 //This is for the post event of the index page
-Route::post('/',array('as' => 'index_page_post', 'before' => 'csrf', 'uses' => 'ImageController@postIndex'));
+Route::post('/', array('as' => 'index_page_post', 'before' => 'csrf', 'uses' => 'ImageController@postIndex'));
+
+//This is to show the image's permalink on our website
+Route::get('snatch/{id}', array('as' => 'get_image_information', 'uses' => 'ImageController@getSnatch'))->where('id', '[0-9]+');
+
+//This route is to show all images.
+Route::get('all',array('as'=>'all_images','uses'=>'ImageController@getAll'));
+
+//This route is to delete the image with given ID
+Route::get('delete/{id}', array('as' => 'delete_image', 'uses' => 'ImageController@getDelete'))->where('id', '[0-9]+');
